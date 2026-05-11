a698b1b7e83cc2eca9512c8fa281da2b10ee8676
Anohoshi <kornienkostas22@gmail.com>
2026-05-11 17:20:09 +0300

    Refactoring the constructor and adding functionality for racket movement

--- pong/src/game.cpp
+++ pong/src/game.cpp
@@ -2,12 +2,14 @@
 #include "ball.hpp"
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <SFML/Window/WindowEnums.hpp>
 
-Game::Game() : vm_(sf::VideoMode::getDesktopMode()), window_(vm_, "Pong", sf::State::Fullscreen), leftBat_({50, windowHeight / 2.f}, 200, sf::Color::White), rightBat_({windowWidth - 50, windowHeight / 2.f}, 200, sf::Color::White), ball_({windowWidth / 2.f, windowHeight / 2.f}, {200.f, 200.f}) {
-  view_.setSize({windowWidth, windowHeight});
-  view_.setCenter({windowWidth / 2.f, windowHeight / 2.f});
+Game::Game() : vm_(sf::VideoMode::getDesktopMode()), window_(vm_, "Pong", sf::State::Fullscreen), leftBat_({50, vm_.size.y / 2.f}, 200, sf::Color::White), rightBat_({vm_.size.x - 50.f, vm_.size.y / 2.f}, 200, sf::Color::White), ball_({vm_.size.x / 2.f, vm_.size.y / 2.f}, {200.f, 200.f}) {
+  view_.setSize({static_cast<float>(vm_.size.x), static_cast<float>(vm_.size.y)});
+  view_.setCenter({static_cast<float>(vm_.size.x) / 2.f, static_cast<float>(vm_.size.y) / 2.f});
   window_.setView(view_);
 }
 
@@ -18,12 +20,33 @@ void Game::processInput() {
     } else if (const auto* key = event->getIf<sf::Event::KeyPressed>()) {
       if (key->scancode == sf::Keyboard::Scancode::Escape)
         window_.close();
+      if (key->scancode == sf::Keyboard::Scancode::Space)
+        paused = !paused;
     }
   }
 }
 void Game::update(sf::Time dt) {
   if (paused)
     return;
+
+  // Left bat
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W)) {
+    if (leftBat_.getBounds().position.y > 0) // не уехала за верх
+      leftBat_.update(dt, -1);
+  } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::S)) {
+    if (leftBat_.getBounds().position.y + leftBat_.getBounds().size.y < vm_.size.y) // не уехала за низ
+      leftBat_.update(dt, 1);
+  }
+
+  // Right bat
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Up)) {
+    if (rightBat_.getBounds().position.y > 0)
+      rightBat_.update(dt, -1);
+
+  } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Down)) {
+    if (rightBat_.getBounds().position.y + rightBat_.getBounds().size.y < vm_.size.y) // не уехала за низ
+      rightBat_.update(dt, 1);
+  }
 
   ball_.update(dt);
 }
